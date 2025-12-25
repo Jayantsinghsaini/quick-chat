7da994f8e450d56fa95b8cc3c294205757eef70f
Jayantsinghsaini <146630172+Jayantsinghsaini@users.noreply.github.com>
2025-12-26 01:34:27 +0530

    Add toggle functionality for sidebar menu

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,6 +13,8 @@ const Sidebar = () => {
 
     const [input, setInput] = useState(false)
 
+    const [open, setOpen] = useState(false);
+    
     const navigate = useNavigate();
 
     const filteredUsers = input ? users.filter((user)=>user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
@@ -26,13 +28,15 @@ const Sidebar = () => {
       <div className='pb-5'>
         <div className='flex justify-between items-center'>
             <img src={assets.logo} alt="logo" className='max-w-40' />
-            <div className="relative py-2 group">
-                <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer' />
-                <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
-                    <p onClick={()=>navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
-                    <hr className="my-2 border-t border-gray-500" />
-                    <p onClick={()=> logout()} className='cursor-pointer text-sm'>Logout</p>
-                </div>
+            <div className="relative py-2">
+                <img src={assets.menu_icon} alt="Menu" className='max-h-5 cursor-pointer' onClick={() => setOpen(prev => !prev)} />
+                {open && (
+                 <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100'>
+                       <p onClick={() => { navigate('/profile'); setOpen(false); }} className='cursor-pointer text-sm' >Edit Profile</p>
+                      <hr className="my-2 border-t border-gray-500" />
+                      <p onClick={() => { logout(); setOpen(false); }} className='cursor-pointer text-sm' >Logout</p>
+                 </div>
+             )}
             </div>
         </div>
 
