c4f0f5068acc1845ceb61a3060b6372b6970d627
Jayantsinghsaini <146630172+Jayantsinghsaini@users.noreply.github.com>
2025-12-27 23:00:10 +0530

    Update ChatContainer.jsx

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -71,7 +71,7 @@ const ChatContainer = () => {
                 {msg.image ? (
                     <img src={msg.image} alt="" className='max-w-[230px] border border-gray-700 rounded-lg overflow-hidden mb-8'/>
                 ):(
-                    <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white ${msg.senderId === authUser._id ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
+                    <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 bg-violet-500/30 text-white ${msg.senderId === authUser._id ? 'rounded-br-none' : 'rounded-bl-none'}`}>{msg.text}</p>
                 )}
                 <div className="text-center text-xs">
                     <img src={msg.senderId === authUser._id ? authUser?.profilePic || assets.avatar_icon : selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-7 h-7 rounded-full object-contain border border-gray-900 bg-black' />
