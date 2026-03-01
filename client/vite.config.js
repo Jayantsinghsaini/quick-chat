512068aaec3e6f355ab2738818212ad19fb08311
Jayantsinghsaini <146630172+Jayantsinghsaini@users.noreply.github.com>
2026-03-01 17:25:57 +0530

    Integrate VitePWA plugin for PWA functionality
    Added VitePWA plugin configuration for PWA support, including service worker auto-update and manifest details.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically updates the service worker
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Quick Chat',
+        short_name: 'Quick Chat',
+        description: 'this is quick chat app for chatting',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable' // Required for some platforms
+          }
+        ]
+      }
+    }),
+  ],
 })
